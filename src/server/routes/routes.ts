b6cf5c53086277da7566c52e835873a4ebcd8c81
unknown <gustavo.carbonera@atplus.com.br>
2026-02-18 17:42:21 -0300

    post

--- src/server/routes/routes.ts
+++ src/server/routes/routes.ts
@@ -3,12 +3,10 @@ import { logEnergyRoute } from "./api/log-energy";
 import { apiIndex } from "./api/api-index";
 import { energyLogsRoute } from "./api/energy-logs";
 import { appRoute } from "./app/appRoute";
-import { viewEnergyPageRoute } from "./app/view-energy-page";
 
 function createRoutes(app: Express) {
 	//? app
 	app.get("/", appRoute);
-	app.get("/view", viewEnergyPageRoute);
 
 	//| api
 	app.get("/api", apiIndex);
