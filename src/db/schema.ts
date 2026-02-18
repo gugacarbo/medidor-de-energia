c3f5cdfefdeffaf1cb5f1bfce93fe538da295df0
unknown <gustavo.carbonera@atplus.com.br>
2026-02-18 17:01:48 -0300

    db

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,7 @@
-import { integer, jsonb, pgTable } from "drizzle-orm/pg-core";
+import { integer, jsonb, pgTable, timestamp } from "drizzle-orm/pg-core";
 
 export const energyLogTable = pgTable("energy_log", {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
 	data: jsonb("data").notNull(),
+	createdAt: timestamp("createdAt").notNull().defaultNow(),
 });
