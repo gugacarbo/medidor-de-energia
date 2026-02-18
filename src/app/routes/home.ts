870d52aac95b927837411d46657eeedb911cd953
unknown <gustavo.carbonera@atplus.com.br>
2026-02-18 16:59:55 -0300

    count

--- src/app/routes/home.ts
+++ src/app/routes/home.ts
@@ -1,9 +1,13 @@
 import { type Request, type Response } from "express";
+import { db } from "../../db/db";
+import { energyLogTable } from "../../db/schema";
 
 async function homeRoute(req: Request, res: Response) {
+	const count = await db.$count(energyLogTable);
 	res.json({
 		status: "ok",
 		message: "API de medidor de energia funcionando!",
+		count,
 	});
 }
 
